3e5db9b4f82971e462c3febdbe7c62ab2f08eca6
Abhishesh <107037012+abhisheshbasnet0@users.noreply.github.com>
2024-11-28 10:33:30 +0545

    Add files via upload

---
+++ backup.php
@@ -0,0 +1,97 @@
+<?php
+require_once __DIR__ . '/../../App/Controller/BackupController.php';
+
+$backup = new BackupController();
+$income_backup = $backup->get_income_backup();
+$expenses_backup = $backup->get_expenses_backup();
+$backup->close_db_connection();
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    <link rel="stylesheet" href="../CSS/dashboard.css">
+</head>
+<body>
+<div class="container">
+    <div class="income-backup">
+<h1>Income Backup</h1>
+<table class="transactions-table">
+    <tr>
+        <th>Category</th>
+        <th>Amount</th>
+        <th>Remarks</th>
+        <th>Inserted Date</th>
+        <th>Inserted Time</th>
+        <th>Action</th>
+    </tr>
+    <?php
+    if($income_backup ==null) {
+        echo "<tr><td>No data found</td></tr>";
+    } else {
+        foreach ($income_backup as $income) {
+            echo "<tr>";
+            echo "<td>" . $income['category_name'] . "</td>";
+            echo "<td>" . $income['income_amount'] . "</td>";
+            echo "<td>" . $income['remarks'] . "</td>";
+            echo "<td>" . $income['date'] . "</td>";
+            echo "<td>" . $income['time'] . "</td>";
+            echo "<td>";
+
+            echo "<form method='post' action='../../App/Controller/BackupController.php'>";
+            echo "<input type='number' value='" . $income['income_id'] . "' name='income-id' hidden>";
+            echo "<button type='submit' name='submit' value='income-backup'>Backup Now</button>";
+            echo "</form>";
+            echo "</td>";
+            echo "</tr>";
+        }
+    }
+    ?>
+</table>
+    </div>
+
+    <div class="expense-backup">
+
+<h1>Expenses Backup</h1>
+<table class="transactions-table">
+    <tr>
+        <th>Category</th>
+        <th>Amount</th>
+        <th>Remarks</th>
+        <th>Inserted Date</th>
+        <th>Inserted Time</th>
+        <th>Action</th>
+    </tr>
+    <?php
+    if($expenses_backup ==null) {
+        echo "<tr><td>No data found</td></tr>";
+    } else {
+
+
+        foreach ($expenses_backup as $expenses) {
+            echo "<tr>";
+            echo "<td>" . $expenses['category_name'] . "</td>";
+            echo "<td>" . $expenses['expenses_amount'] . "</td>";
+            echo "<td>" . $expenses['remarks'] . "</td>";
+            echo "<td>" . $expenses['date'] . "</td>";
+            echo "<td>" . $expenses['time'] . "</td>";
+            echo "<td>";
+
+            echo "<form method='post' action='../../App/Controller/BackupController.php'>";
+            echo "<input type='number' value='" . $expenses['expenses_id'] . "' name='expenses-id' hidden>";
+            echo "<button type='submit' name='submit' value='expenses-backup'>Backup Now</button>";
+            echo "</form>";
+            echo "</td>";
+            echo "</tr>";
+        }
+    }
+    ?>
+</table>
+    </div>
+
+</div>
+</body>
+</html>
